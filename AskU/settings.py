206a3251d6bce227ff907b8ece7deea632c74754
Anatoliy <aronuz1@hotmail.com>
2021-10-04 11:34:04 -0400

    Questions app

--- AskU/settings.py
+++ AskU/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'djoser',
     'widget_tweaks',
 
-    'users'
+    'users',
+    'questions'
 ]
 
 MIDDLEWARE = [
---
+++ questions/__init__.py
@@ -0,0 +1,2 @@
+# Not needed fr Django >= 3.2
+default_app_config = "questions.app.QuestionsConfig"
